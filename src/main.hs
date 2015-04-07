dc0227f935d5fd0bd354dcde95961ca2b25950c0
ayu-mushi <ayu.mushi2015@gmail.com>
2015-04-07 09:58:27 +0900

    delete examples

--- src/main.hs
+++ src/main.hs
@@ -34,29 +34,6 @@ mapGenomesToWorld1 r0 gss = toGrPp $
 world1 :: StdGen -> GraphPaper
 world1 = mapGenomesToWorld1 `flip` replicate 10 ancestor1
 
-justGenomesExample0 :: IO ()
-justGenomesExample0 = putStrLn $ concat $ intersperse "\n\n" $ map (concat <<< (intersperse " ") <<< (unasm $ nameSet namedInsts1) <<< elems) $ genericAlgorithm (instSet namedInsts1) 50 100 10 (mapGenomesToWorld1 (mkStdGen 324243)) (replicate 10 ancestor1)
-
--- methods of experiments
--- Axelrod「An Evolutionary Approach to Norm」風に、報酬の有無によって協調(情報提供)率が上がるかを調べる
-experiment1 :: IO ()
-experiment1 = putStrLn $
-  (show $ map (\r0 -> (popularityOfTheGene
-    (genericAlgorithm
-      ((instSet namedInsts1)
-        // [(numberOfInst namedInsts1 "rewardU", id),
-          (numberOfInst namedInsts1 "rewardD", id),
-          (numberOfInst namedInsts1 "rewardL", id),
-          (numberOfInst namedInsts1 "rewardR", id)])
-      50 10 10 (mapGenomesToWorld1 (mkStdGen r0)) (replicate 10 ancestor1)))
-      $ numberOfInst namedInsts1 "mentionU") [143243..143253])
-  ++ "\n" ++
-  (show $ map (\r0 -> popularityOfTheGene
-    (genericAlgorithm
-      (instSet namedInsts1)
-      50 10 10 (mapGenomesToWorld1 (mkStdGen r0)) (replicate 10 ancestor1))
-      $ numberOfInst namedInsts1 "mentionU") [45823..45833])
-
 data Options = Options
   { interactive :: Bool
   , inputFile :: String
